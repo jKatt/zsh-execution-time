d50adcf6365ad6ba95c9f06fecfa77c1600cc9ed
Jan Kattelans <jan.kattelans@googlemail.com>
2017-10-29 13:28:58 +0100

    change if-else block

--- execution-time.plugin.zsh
+++ execution-time.plugin.zsh
@@ -8,14 +8,13 @@ function precmd() {
     elapsed=$(($now-$timer))
     unit="ms"
     color=240
-    if [ $elapsed -ge 1000 ]; then
-      elapsed=$(($elapsed/1000))
-      unit="s"
-    fi
-    if [ $elapsed -ge 60 ]; then
-      elapsed=$(($elapsed/60))
+    if [[ $elapsed -ge 60000 ]]; then
+      elapsed=$(($elapsed/60000))
       unit="m"
       color="red"
+    elif [[ $elapsed -ge 3000 ]]; then
+      elapsed=$(($elapsed/1000))
+      unit="s"
     fi
     export RPROMPT="%F{${color}}${elapsed}${unit}%F{240} < ${dateString}%f"
     unset timer
