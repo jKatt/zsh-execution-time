8bed43e6cfe2d847c73819e7e03e48666523cd16
Jan Kattelans <jan.kattelans@googlemail.com>
2018-06-26 15:07:05 +0200

    add hours

--- execution-time.plugin.zsh
+++ execution-time.plugin.zsh
@@ -3,11 +3,16 @@ function preexec() {
 }
 function precmd() {
   dateString=$(gdate +"%a %b %d %T %Y")
-  if [ $timer ]; then
+  if [[ $timer ]]; then
     now=$(gdate +%s%3N)
     elapsed=$(($now-$timer))
+    elapsedHours=""
     unit="ms"
     color=240
+    if [[ $elapsed -ge 3600000 ]]; then
+      hours=$(($elapsed/3600000))
+      elapsedHours="${hours}h "
+    fi
     if [[ $elapsed -ge 60000 ]]; then
       elapsed=$(($elapsed/60000))
       unit="m"
@@ -16,7 +21,7 @@ function precmd() {
       elapsed=$(($elapsed/1000))
       unit="s"
     fi
-    export RPROMPT="%F{${color}}${elapsed}${unit}%F{240} < ${dateString}%f"
+    export RPROMPT="%F{${color}}${elapsedHours}${elapsed}${unit}%F{240} < ${dateString}%f"
     unset timer
   else
     export RPROMPT="%F{240}${dateString}%f"
